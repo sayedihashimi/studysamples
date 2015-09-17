b1d4ee1ed3e1e1be36d738b4e4397244c029af18
Sayed Ibrahim Hashimi <sayed.hashimi@gmail.com>
2015-09-17 19:17:26 +0000

    adding person.cs for step 2

---
+++ WebApplication1/src/WebApplication1/Models/Person.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class Person
+    {        
+        public int Id { get; set; }
+
+        [Required]
+        public string FirstName { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+    }
+}
